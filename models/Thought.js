d6c60f091a0c7db1b1581854527efd30d77a5cfd
Nevah <165732847+nevah-evans@users.noreply.github.com>
2024-08-24 14:30:51 -0500

    adding Reaction.js model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const {  Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+// const formatDate = new Date();
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => new Date(timestamp).toLocaleDateString(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function (){
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports= Thought;
